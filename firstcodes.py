5b0d2e6344901c3cd610bdaad9aaec2d23d38805
Suna Yıldız <suna12898@icloud.com>
2026-01-20 13:47:54 +0300

    Update firstcodes.py with variable assignments and prints

--- firstcodes.py
+++ firstcodes.py
@@ -1,7 +1,8 @@
+# You can start learning by first typing "Hello World" 
 print("Hello World ");
+# Assign a value to a variable,then print his value to the screen
 a=5;
 print(a);
-
 
 numara=2018;
 print(numara);
@@ -10,6 +11,9 @@ print(dil);
 pi=3.14;
 print(pi);
 sayi=5;
+
+#print all variables with a single print command
+
 AdiSoyadi="Suna Yıldız";
 Personel_No=123456789;
 kullanici="Öğrenci";
@@ -20,6 +24,10 @@ ogrenciBolum_no=202313171049;
 sehir="Aydın";
 Sehir="İzmir";
 print(sayi,AdiSoyadi,Personel_No,kullanici,Sayi1,sayi_2,OGR_NO,ogrenciBolum_no,sehir,Sehir);
+
+# Separating characters using the print() function,
+# demonstrating the use of a custom separator (sep) and line end (end) control
+
 print("Çukurova","Üniversitesi",sep="*");
 print("P","y","t","h","o","n",sep=".");
 print(*"Python",sep=".");
@@ -33,10 +41,14 @@ print(ad,soyadi,sehir,yil,sep="\n");
 print("Suna\nYıldız\n20 yasinda");
 print("bir","iki","üç",sep="\t");
 print("Çukurova","Üniversitesi",sep="\n\n");
+
+# Printing characters on the screen using their Unicode (\uXXXX) and Hex (\xXX) codes
 print("\u0068");
 print("\x45");
 print("\u0065");
 print("\x67");
+
+# Converting to different data types and redefining variables
 a=45;
 a=str(a);
 print(type(a));
@@ -45,6 +57,8 @@ print(int(a))
 b="suna";
 a=b
 print(a)
+
+# Variable declaration in Python, separating with underscores, and printing lines one below the other
 Ad="Suna"
 Soyad="Yıldız"
 Bölüm="Bilgisayar Mühendisliği"
@@ -55,6 +69,8 @@ a="Adana"
 b="Mersin"
 c="Hatay"
 print(a,b,c,sep="\n")
+
+# Calculating monthly travel expenses using variables and printing them to the screen
 Adı="Suna"
 Soyadi="Yıldız"
 Gidis=1.5
@@ -62,5 +78,6 @@ Dönüs=1.5
 AyGun=20
 YolUcret=(Gidis+Dönüs)*20
 print(Adı,Soyad,"günde",(Gidis+Dönüs),"TLden",AyGun,"günde aylık yol masrafı",YolUcret,"olmaktadır.")
+
 
 
