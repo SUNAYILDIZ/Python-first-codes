2bbc8c3384a0b5e3344a92df9f0acd88f5d7cc75
Suna Yıldız <suna12898@icloud.com>
2026-01-20 13:59:28 +0300

    Refactor code into functions for better structure

--- ödev.py
+++ ödev.py
@@ -1,31 +1,43 @@
 # Grade evaluation program using conditional statements
-not1 = float(input("Notunuzu giriniz:"))
-if not1>100:
-    print("Hatalı girdiniz")
-elif 90<not1<=100:
-    print("Notunuz A")
-elif 80<not1<=90:
-    print("Notunuz B")
-elif 70<not1<=80:
-    print("Notunuz C")
-else:
-    print("Notunuz F")
-    
+def grade_evaluation():
+    not1 = float(input("Notunuzu giriniz: "))
+    if not1 > 100:
+        print("Hatalı girdiniz")
+    elif 90 < not1 <= 100:
+        print("Notunuz A")
+    elif 80 < not1 <= 90:
+        print("Notunuz B")
+    elif 70 < not1 <= 80:
+        print("Notunuz C")
+    else:
+        print("Notunuz F")
+
+
 # Program that checks whether the entered number is divisible by both 3 and 7 at the same time
-sayi = float(input("Bir sayi giriniz: "))
-if sayi%3==0 and sayi%7==0:
-    print("Sayi hem 3've hem 7' ye bölünebiliyor.")
-else:
-     print("Sayi bölünemez.")
+def divisibility_check():
+    sayi = float(input("Bir sayi giriniz: "))
+    if sayi % 3 == 0 and sayi % 7 == 0:
+        print("Sayi hem 3'e hem 7'ye bölünebiliyor.")
+    else:
+        print("Sayi bölünemez.")
+
 
 # Evaluating air temperature using conditional statements
-havaSicakligi=int(input("Hava sıcaklıgını giriniz: "))
-if havaSicakligi>0:
-    print("Hava sıcak")
-elif havaSicakligi<0:
-    print("Hava soguk")
-else:
-    print("Hava sicakligi 0")
+def temperature_evaluation():
+    havaSicakligi = int(input("Hava sıcaklığını giriniz: "))
+    if havaSicakligi > 0:
+        print("Hava sıcak")
+    elif havaSicakligi < 0:
+        print("Hava soğuk")
+    else:
+        print("Hava sıcaklığı 0")
+
+
+if __name__ == "__main__":
+    grade_evaluation()
+    divisibility_check()
+    temperature_evaluation()
 
     
+
 
